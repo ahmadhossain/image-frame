b191c90aa1ded3bb459c1284dd82da455b5a5d18
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2023-11-24 23:05:29 +0600

    responsive of grid row (static)

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
-// function handleResize() {
-//   const windowWidth = window.innerWidth - 128;
+function handleResize() {
+  //   const windowWidth = window.innerWidth - 128;
+  // console.log(windowWidth);
 
-//   const div = document.querySelector(".grid-container");
-//   // console.log(windowWidth);
+  const div = document.querySelector(".grid-container");
+  const gridItem = document.querySelector(".item");
+  const textNode = document.querySelector(".text");
 
-//   const numOfCol = Math.round(windowWidth / 100);
-//   let gridCol = "";
-//   for (let i = 0; i < numOfCol; i++) {
-//     gridCol += " auto";
-//   }
-//   // console.log(numOfCol);
+  //   const numOfCol = Math.round(windowWidth / 100);
+  //   let gridCol = "";
+  //   for (let i = 0; i < numOfCol; i++) {
+  //     gridCol += " auto";
+  //   }
+  // console.log(numOfCol);
 
-//   const txtNode = document.querySelector("#text");
-//   const offsetHeight = txtNode.offsetHeight;
+  const textHeight = textNode.offsetHeight;
+  const itemHeight = gridItem.offsetHeight;
 
-//   let numOfRow = Math.ceil(offsetHeight / 100);
-//   numOfRow += 2;
-//   let gridRow = "";
-//   console.log("offsetHeight: " + txtNode.offsetHeight);
-//   console.log("numOfRow: " + numOfRow);
+  if (textHeight % itemHeight > 5) {
+    let numOfRow = Math.ceil(textHeight / itemHeight);
+    numOfRow += 2;
+    let gridRow = "";
 
-//   for (let i = 0; i < numOfRow; i++) {
-//     gridRow += " 1fr";
-//   }
+    for (let i = 0; i < numOfRow; i++) {
+      gridRow += " 1fr";
+    }
 
-//   div.style.display = "grid";
-//   // div.style.gridTemplateColumns = gridCol;
-//   div.style.gridTemplateRows = gridRow;
-//   div.style.gap = "10px";
-//   div.style.padding = "1px";
-//   div.style.backgroundColor = "#2196f3";
-
-//   console.log("numOfRow" + gridRow);
-// }
-
-const txtNode = document.querySelector(".text");
-console.log(txtNode.offsetWidth);
+    div.style.gridTemplateRows = gridRow;
+    textNode.style.gridArea = "2 / 2 / 4 / 9";
+  }
+  console.log("th " + textHeight);
+  console.log("gh " + itemHeight);
+}
