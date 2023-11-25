9a19e078aea88b32843f9425b0b8fe5782b8da36
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2023-11-25 14:17:37 +0600

    -Responsive Column -Extra div added

--- index.js
+++ index.js
@@ -1,35 +1,59 @@
 function handleResize() {
-  //   const windowWidth = window.innerWidth - 128;
-  // console.log(windowWidth);
-
   const gridDiv = document.querySelector(".grid-container");
   const gridItem = document.querySelector(".item");
   const textDiv = document.querySelector(".text-div");
   const textNode = document.querySelector(".text");
+  const extraDiv = document.querySelectorAll(".extra");
 
+  //   const windowWidth = window.innerWidth - 128;
   //   const numOfCol = Math.round(windowWidth / 100);
-  //   let gridCol = "";
-  //   for (let i = 0; i < numOfCol; i++) {
-  //     gridCol += " auto";
-  //   }
-  // console.log(numOfCol);
 
   const textHeight = textNode.offsetHeight;
   const itemHeight = gridItem.offsetHeight;
 
-  //   64 --> padding (4rem), 5 --> ignoring the minimum difference
-  if (Math.abs(textHeight + 64 - itemHeight) > 2) {
+  //   64 --> padding (4rem)
+  if (Math.abs(textHeight + 64 - itemHeight) > 0) {
     let numOfRow = Math.ceil((textHeight + 64) / itemHeight);
     numOfRow += 2;
     let gridRow = "";
+    let gridCol = "";
 
-    for (let i = 0; i < numOfRow; i++) {
-      gridRow += " 1fr";
+    console.log(extraDiv);
+
+    if (numOfRow === 4) {
+      extraDiv[0].style.display = "flex";
+      extraDiv[1].style.display = "flex";
+    } else if (numOfRow === 3) {
+      extraDiv[0].style.display = "none";
+      extraDiv[1].style.display = "none";
     }
 
+    for (let i = 0; i < numOfRow; i++) gridRow += " 1fr";
+    //  updating Row
     gridDiv.style.gridTemplateRows = gridRow;
-    textDiv.style.gridArea = `2 / 2 / ${numOfRow} / 9`;
-    console.log("text size changed");
+    textDiv.style.gridRow = `2 / ${numOfRow} `;
+
+    // updation Column
+    if (itemHeight <= 60) {
+      for (let i = 0; i < 8; i++) gridCol += " minmax(50px, 1fr)";
+
+      gridDiv.style.gridTemplateColumns = gridCol;
+      textDiv.style.gridColumn = `2 / 8`;
+    } else if (itemHeight > 70 && itemHeight < 85) {
+      for (let i = 0; i < 9; i++) gridCol += " minmax(50px, 1fr)";
+
+      gridDiv.style.gridTemplateColumns = gridCol;
+      textDiv.style.gridColumn = `2 / 9`;
+
+      // extraDiv[0].style.display = "none";
+      // extraDiv[1].style.display = "none";
+    } else if (itemHeight > 95) {
+      for (let i = 0; i < 10; i++) gridCol += " minmax(50px, 1fr)";
+
+      gridDiv.style.gridTemplateColumns = gridCol;
+      textDiv.style.gridColumn = `2 / 10`;
+    }
+
     console.log(Math.abs(textHeight + 64 - itemHeight));
   }
 
